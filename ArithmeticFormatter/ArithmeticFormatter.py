878882e5f267dcea3b5269a51342df834fb2fef3
CptAwe <georgepapoulias@gmail.com>
2021-06-22 08:59:39 +0300

    initial commit

---
+++ ArithmeticFormatter/ArithmeticFormatter.py
@@ -0,0 +1,9 @@
+def arithmetic_arranger(problems: list) -> str:
+
+  arranged_problems = ""
+
+  return arranged_problems
+
+
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+
